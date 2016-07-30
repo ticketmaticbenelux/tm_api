3f9fa3dd384e333bc5ac9399f96be62153e537d5
Rutger Weemhoff <rutger@direct-control.nl>
2016-07-30 16:08:37 +0200

    Updates test to recent changes

--- test/test.js
+++ test/test.js
@@ -2,15 +2,15 @@
 
 var env = require('node-env-file')
 
-env(__dirname + '/../.env');
+env(__dirname + '/../.env')
 
 var api = require("../tm_api.js")
 
-api.setAuthorization({
+var client = {
 	shortname: process.env.SHORTNAME,
 	key: process.env.API_KEY,
 	secret: process.env.API_SECRET
-})
+}
 
-api.get("contacts")
+api.getList(client, "contacts")
 .then(r => console.log(r), e => console.log(e))
